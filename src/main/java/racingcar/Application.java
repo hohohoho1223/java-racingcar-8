96fce63466303be936d53c98e154db82b46ce06a
hohohoho1223 <dnjsgh1820@naver.com>
2025-10-27 20:30:02 +0900

    feat(app): 애플리케이션 실행 플로우 연결
    - InputView로 입력 수집, RacingGame 실행, OutputView로 결과 출력
    - 요구사항에 맞춰 System.exit() 미사용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import java.util.List;
+
+import racingcar.domain.RacingGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingGame racingGame = new RacingGame();
+
+        List<String> carNames = InputView.readCarNames();
+        int tryCount =InputView.readTryCount();
+
+        racingGame.start(carNames, tryCount);
+
+        OutputView.printWinners(racingGame.getWinners());
     }
 }

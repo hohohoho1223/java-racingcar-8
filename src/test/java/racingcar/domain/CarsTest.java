fe9ebb694ef6e79486643a64362b68aba44b6352
hohohoho1223 <dnjsgh1820@naver.com>
2025-10-27 23:21:06 +0900

    test(domain): Cars 단위 테스트 케이스 추가
    - 생성자가 전달받은 이름 리스트로 객체를 잘 생성 테스트
    - 단독 우승자만 반환 테스트

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -8,7 +8,16 @@ import static org.assertj.core.api.Assertions.*;
 class CarsTest {
 
     @Test
-    @DisplayName("")
+    @DisplayName("생성자가 전달받은 이름 리스트로 객체를 잘 생성했는지")
+    void constructor_creates_cars_list_correctly() {
+        Cars cars = new Cars(List.of("pobi", "woni"));
+        assertThat(cars.getCars())
+                .extracting(Car::getName)
+                .containsExactly("pobi", "woni");
+    }
+
+    @Test
+    @DisplayName("랜덤값과 기준에 따라 각각의 자동차가 이동하는지")
     void moveAll_moves_each_car_with_corresponding_random() {
         Cars cars = new Cars(List.of("a","b","c"));
         cars.moveAll(List.of(4, 3, 9)); // a:+1, b:+0, c:+1
@@ -23,6 +32,15 @@ class CarsTest {
     }
 
     @Test
+    @DisplayName("단독 우승자만 반환하는지")
+    void findWinners_returns_single_winner() {
+        Cars cars = new Cars(List.of("pobi","woni","jun"));
+        cars.moveAll(List.of(4, 3, 3)); // a:+1, b:+0, c:+0
+        assertThat(cars.findWinners()).containsExactly("pobi");
+    }
+
+    @Test
+    @DisplayName("공동 우승자가 있으면 반환하는지")
     void findWinners_returns_all_ties() {
         Cars cars = new Cars(List.of("pobi","woni","jun"));
         cars.moveAll(List.of(4, 4, 3)); // pobi:+1, woni:+1, jun:+0
